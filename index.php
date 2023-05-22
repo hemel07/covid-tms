5ea6f4b68de02d9204acf64d3e94e88fa8531d79
Hemel Sarker Shuvo <hemelsarkershuvo@gmail.com>
2023-05-23 00:04:30 +0600

    covid management system this can be use as any pandemic management system or it also can use as pathology laboratory management system

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid-19 TMS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About Coronavirus </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Covid-19 Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>COVID-19 TMS</h1>
+      <p class="lead">COVID-19  Testing Management System</p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>About this page</h2>
+          <p class="lead">Coronaviruses are a viral type. There are numerous types, some of which cause sickness. COVID-19 is a pandemic of respiratory sickness caused by SARS-CoV-2, a coronavirus discovered in 2019.</p>
+          <p class="lead">The coronavirus is disseminated through droplets and virus particles released into the air when an infected person breathes, talks, laughs, sings, coughs, or sneezes, according to researchers. Larger droplets may fall to the ground in a matter of seconds, but microscopic infectious particles can persist in the air and concentrate in enclosed spaces, particularly where a large number of people are assembled and ventilation is poor. COVID-19 prevention necessitates the use of masks, hand cleanliness, and physical separation.</p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+<p><strong>1. Cough</strong><br /> 
+          <hr />
+<p><strong>2. Fever or chills</strong><br /> 
+          <hr />
+<p><strong>3. Shortness of breath or difficulty breathing</strong><br /> 
+          <hr />
+<p><strong>4. Muscle or body aches</strong><br /> 
+          <hr />
+<p><strong>5. Sore throat</strong><br />
+          <hr />
+<p><strong>6. New loss of taste or smell</strong><br />
+          <hr />
+<p><strong>7. Diarrhea and vomiting</strong><br />
+          <hr />
+<p><strong>8. Headache and runny nose</strong><br />
+</p>
+<p>Some persons who are infected with the coronavirus develop minor COVID-19 symptoms, while others show no symptoms at all. COVID-19, on the other hand, can cause respiratory failure, long-term lung, and heart muscle damage, nervous system difficulties, kidney failure, and death in certain people.</p>
+          <hr />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li>Get Vaccinated</li>
+            <li>Wear a mask</li>
+            <li>Stay 6 feet away from others</li>
+            <li>Avoid crowds and poorly ventilated spaces</li>
+            <li>Wash your hands often</li>
+            <li>Cover coughs and sneezes</li>
+            <li>Clean and disinfect</li>
+            <li>Monitor your health daily</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white"></p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
